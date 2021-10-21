6e9529ef4a807b0c3d332be692f675d29988d2e6
Vinayak Garg <vinayak_g@ch.iitr.ac.in>
2021-10-21 19:56:43 +0530

    footer and contactus

--- src/Component/Contact-Us.jsx
+++ src/Component/Contact-Us.jsx
@@ -1,52 +1,60 @@
 import react from "react";
 import './Contact-Us.css';
-
+import akshat from "./Website Assets/akshat.png";
+import muskan from "./Website Assets/muskan.png";
+import Anvesh from "./Website Assets/Anvesh (1).png";
 function ContactUs() {
     return (
   <div>
-    <h3 className="text-1">Contact Us</h3>
+    <h3 className="text-11">Contact Us</h3>
 <div className="Contact-Us">
     <div className="form"> 
-        <form action="mailto:contact@yourdomain.com"
+        <form action="mailto:civilconclave@iitr.ac.in"
 method="POST"
 enctype="multipart/form-data"
 name="EmailForm">
     
     <input type="text" size="19" className="Name" name="Contact-Name" placeholder="Full Name"/>
    <br/><br/>
-    <input type="email" name="Contact-Email" placeholder="Email ID"/>
+    <input type="email" className="Name" name="Contact-Email" placeholder="Email ID"/>
    
     <br/><br/>
-    <button type="submit" value="Submit">Send</button>
+    <input className="Message" type="Message" placeholder="Type your message here..." />
+    <br/><br/>
+    <button type="submit" className="Button" value="Submit">SEND MESSAGE</button>
         </form>  
         
         
          </div>
     <div>
         <div className="people">
-            <div></div>
-            <div>
-                Anvesh Chauhan<br/>
-Overall Coordinator<br/>
-achauhan2@ce.iitr.ac.in
+            <div className="Image">
+                <img  className="Image" src={Anvesh} alt="" />
+            </div>
+            <div className="people-text" >
+              <div className="people-text-1">  Anvesh Chauhan</div>
+<div  className="people-text-2">Overall Coordinator<br/>
+achauhan2@ce.iitr.ac.in</div>
 </div>
         </div>
         <div className="people">
-            <div>
-                <img src={process.env.PUBLIC_URL + " "} alt="" />
+            <div className="Image">
+                <img  className="Image" src={muskan} alt="" />
             </div>
-            <div>Muskan Dogra<br/>
-Overall Co-Coordinator<br/>
-mdogra@ce.iitr.ac.in
+            <div className="people-text"><div  className="people-text-1">Muskan Dogra</div>
+<div  className="people-text-2">Overall Co-Coordinator<br/>
+mdogra@ce.iitr.ac.in</div>
                
             </div>
         </div>
         <div className="people">
-            <div></div>
-            <div>
-                 Akshat Srivastava<br/>
-Overall Co-Coordinator<br/>
-asrivastava@ce.iitr.ac.in
+            <div className="Image">
+                <img  className="Image" src={akshat} alt="" />
+            </div>
+            <div className="people-text">
+                 <div className="people-text-1">Akshat Srivastava</div>
+<div  className="people-text-2">Overall Co-Coordinator<br/>
+asrivastava@ce.iitr.ac.in</div>
             </div>
         </div>
     </div>
